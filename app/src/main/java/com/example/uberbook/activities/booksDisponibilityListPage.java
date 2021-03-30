59de34be61377816ad5c2843151077ee4871d4cf
kevin lehoux <LehouxKevin>
2021-03-30 17:16:26 +0200

    added activities package + created books disponibility page

---
+++ app/src/main/java/com/example/uberbook/activities/booksDisponibilityListPage.java
@@ -0,0 +1,16 @@
+package com.example.uberbook.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uberbook.R;
+
+public class booksDisponibilityListPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_disponibility_list_page);
+    }
+}
