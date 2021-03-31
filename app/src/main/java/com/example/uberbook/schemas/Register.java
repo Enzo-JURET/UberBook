b8519b836622f1981bd9ac5884090bfaf2ff400d
Telougat <thelorenzo533gmail.com>
2021-03-31 15:59:01 +0100

    Login system

--- app/src/main/java/com/example/uberbook/schemas/Register.java
+++ app/src/main/java/com/example/uberbook/schemas/Register.java
@@ -15,4 +15,28 @@ public class Register {
     @Expose
     @SerializedName("password")
     String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
