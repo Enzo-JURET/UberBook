303394d527379f63931ed6d549305ca206f34f4b
unknown <timothe.laine@gmail.com>
2021-03-31 17:08:11 +0200

    Add zxing lib + implement scan function

--- app/src/main/java/com/example/uberbook/activities/Scan.java
+++ app/src/main/java/com/example/uberbook/activities/Scan.java
@@ -1,4 +1,52 @@
 package com.example.uberbook.activities;
 
-public class Scan {
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.example.uberbook.R;
+import com.example.uberbook.utils.Navigation;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class Scan extends AppCompatActivity {
+
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        context = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+//        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
+//
+//        IntentIntegrator integrator = new IntentIntegrator(this);
+//        integrator.initiateScan();
+
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        Get the scan result
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
+
--- app/src/main/java/com/example/uberbook/utils/Navigation.java
+++ app/src/main/java/com/example/uberbook/utils/Navigation.java
@@ -1,16 +1,21 @@
 package com.example.uberbook.utils;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.uberbook.MainActivity;
 import com.example.uberbook.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum;
 import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
 import com.nightonke.boommenu.BoomButtons.SimpleCircleButton;
@@ -33,6 +38,8 @@ public class Navigation {
                 .listener(new OnBMClickListener() {
                     @Override
                     public void onBoomButtonClick(int index) {
+                        ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
+                        new IntentIntegrator(context).initiateScan();
                         Log.d("Debug", "Clicked on button");
                         //TODO: Action when calling
                     }
@@ -60,5 +67,20 @@ public class Navigation {
                             context.startActivity(intent);
                     }
                 }));
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        Get the scan result
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+
+//                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+//                Toast.makeText(, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+//            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
