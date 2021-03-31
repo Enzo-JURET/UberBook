fc90912f3a7c07df5c9c631aa2aaaaf740df8733
Enzo JURET <juretenzo@gmail.com>
2021-03-31 16:09:14 +0200

    Ajout du menu sur les activités Borrow et Return

--- app/src/main/java/com/example/uberbook/activities/ReturnBook.java
+++ app/src/main/java/com/example/uberbook/activities/ReturnBook.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.uberbook.R;
+import com.example.uberbook.utils.Navigation;
 
 public class ReturnBook extends AppCompatActivity {
 
@@ -12,5 +13,7 @@ public class ReturnBook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_return_book);
+
+        Navigation.init(findViewById(R.id.bmb));
     }
 }
